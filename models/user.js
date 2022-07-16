c487744943082c1806eea331445a659c2ba94d80
Mike Green <greens1593@gmail.com>
2022-07-16 17:13:35 +0300

    Delete card model, modified card router. Cart works

--- models/user.js
+++ models/user.js
@@ -28,4 +28,25 @@ const userSchema = new Schema({
     
 })
 
+userSchema.methods.addToCart = function(course){
+    console.log(course)
+    const clonedItems = [...this.cart.items]
+    const index = clonedItems.findIndex(c =>{
+        return c.courseId.toString() === course._id.toString()
+    })
+
+    if(index >= 0) {
+        clonedItems[index].count += 1 
+    } else {
+        clonedItems.push({
+            courseId: course._id,
+            count: 1
+        })
+    }
+
+    const newCart = {items: clonedItems}
+    this.cart = newCart
+    return this.save()
+}
+
 module.exports = model('User', userSchema)
--- routers/card.js
+++ routers/card.js
@@ -1,11 +1,22 @@
 const {Router} = require('express');
+const Course = require('../models/course');
 const router = Router();
-const Card = require('../models/card')
-const Course = require('../models/course')
+
+function mapCartItems(cart){
+    return cart.items.map( c => ({
+        ...c.courseId._doc, count: c.count
+    }))
+}
+
+function computePrice(courses){
+    return courses.reduce((total, course) =>{
+        return total += course.price * course.count
+    }, 0)
+}
 
 router.post('/add', async (req, res)=>{
-    const course = await Course.getCourseById(req.body.id);
-    const card = await Card.add(course);
+    const course = await Course.findById(req.body.id);
+    await req.user.addToCart(course)
     res.redirect('/card')
 })
 
@@ -26,12 +37,16 @@ router.delete('/remove/:id', async (req, res) =>{
 })
 
 router.get('/', async (req, res) => {
-    const card = await Card.fetch();
+    const user = await req.user
+        .populate('cart.items.courseId')
+
+    const courses = mapCartItems(user.cart)
+
     res.render('card', {
         title: 'Корзина',
         isCard: true,
-        courses: card.courses,
-        price: card.price
+        courses: courses,
+        price: computePrice(courses)
     })
 })
 
