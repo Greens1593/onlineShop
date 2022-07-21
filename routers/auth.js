b51a579ae62f0ece4ffd2bb6c4bbef443dfa6a08
Mike Green <greens1593@gmail.com>
2022-07-21 20:22:14 +0300

    Bcryptjs was added in the app. Password in database is crypt by bcryptjs

--- routers/auth.js
+++ routers/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user.js')
 const router = Router()
 
@@ -20,7 +21,8 @@ router.post('/login', async (req, res) => {
         const {email, password} = req.body;
         const candidate = await User.findOne({email})
         if(candidate){
-            if(candidate.password === password){
+            const areSame = await bcrypt.compare(password, candidate.password) 
+            if(areSame){
                 req.session.user = candidate;
                 req.session.isAuthenticated = true;
                 req.session.save(err => {
@@ -47,8 +49,9 @@ router.post('/register', async (req, res) => {
         if(candidate){
             res.redirect('/auth/login#register')
         } else {
+            const hashPassword = await bcrypt.hash(password, 10)
             const user = new User({
-                email, name, password, cart: {items: []}
+                email, name, password: hashPassword, cart: {items: []}
             })
             await user.save()
             res.redirect('/auth/login#login')
