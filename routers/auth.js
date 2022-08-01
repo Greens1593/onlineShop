131e001a1977d44938b660d9708b2c5754256cf3
Mike Green <greens1593@gmail.com>
2022-08-01 20:25:53 +0300

    In app was added asyncron validator for checking if user has been

--- routers/auth.js
+++ routers/auth.js
@@ -62,18 +62,12 @@ router.post('/login', async (req, res) => {
 
 router.post('/register', registerValidators, async (req, res) => {
     try{
-        const {email, password, confirm, name} = req.body;
-        const candidate = await User.findOne({email})
+        const {email, password, name} = req.body;
         const errors = validationResult(req)
         if(!errors.isEmpty()){
             req.flash('registerError', errors.array()[0].msg)
             return res.status(422).redirect('/auth/login#register')
         }
-
-        if(candidate){
-            req.flash('registerError', 'Пользователь с таким email уже существует')
-            res.redirect('/auth/login#register')
-        } else {
             const hashPassword = await bcrypt.hash(password, 10)
             const user = new User({
                 email, name, password: hashPassword, cart: {items: []}
@@ -81,7 +75,6 @@ router.post('/register', registerValidators, async (req, res) => {
             await user.save()
             res.redirect('/auth/login#login')
             await transporter.sendMail(regEmail(email))
-        }
     }
     catch (e){
         console.log(e)
--- utils/validators.js
+++ utils/validators.js
@@ -1,7 +1,17 @@
 const {body} = require('express-validator')
+const User = require('../models/user.js')
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Введите коректный email'),
+    body('email').isEmail().withMessage('Введите коректный email').custom(async (value, {req})=>{
+        try{
+            const candidate = await User.findOne({email: value})
+            if(candidate) {
+                return Promise.reject('Пользователь с таким email уже существует')
+            }
+        } catch(e){
+            console.log(e)
+        }
+    }),
     body('password', 'Пароль должен состоять не мение чем из 6 символов').isLength({min: 6, max: 56}).isAlphanumeric(),
     body('confirm').custom((value, {req}) => {
         if (value !== req.body.password){
