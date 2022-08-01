80eff48bb4d6c5c0aaf743b3ffd193fee9383803
Mike Green <greens1593@gmail.com>
2022-08-01 20:36:19 +0300

    In file validators.js was added normalize vakidators

--- utils/validators.js
+++ utils/validators.js
@@ -2,7 +2,9 @@ const {body} = require('express-validator')
 const User = require('../models/user.js')
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Введите коректный email').custom(async (value, {req})=>{
+    body('email')
+        .isEmail().withMessage('Введите коректный email')
+        .custom(async (value, {req})=>{
         try{
             const candidate = await User.findOne({email: value})
             if(candidate) {
@@ -11,13 +13,21 @@ exports.registerValidators = [
         } catch(e){
             console.log(e)
         }
-    }),
-    body('password', 'Пароль должен состоять не мение чем из 6 символов').isLength({min: 6, max: 56}).isAlphanumeric(),
-    body('confirm').custom((value, {req}) => {
+    })
+    .normalizeEmail(),
+    body('password', 'Пароль должен состоять не мение чем из 6 символов')
+    .isLength({min: 6, max: 56})
+    .isAlphanumeric()
+    .trim(),
+    body('confirm')
+    .custom((value, {req}) => {
         if (value !== req.body.password){
             throw new Error('Значение в поле "Повторите пароль" должно совпадать со значением в поле "Пароль"')
         }
         return true
-    }),
-    body('name', 'Имя должно состоять не мение чем из трех символов').isLength({min: 3})
+    })
+    .trim(),
+    body('name', 'Имя должно состоять не мение чем из трех символов')
+    .isLength({min: 3})
+    .trim()
 ]
